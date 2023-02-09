2eeb562db1d62d258dc4c4bfd9010b532ff721a0
A. Maslarov <nsite2014@gmail.com>
2023-02-09 11:15:32 +0200

    Added tags cmponent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import "./Tags.js";
+import RenderTags from "./Tags.js";
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
       </section>
       <div class="container is-fullhd">
         <div class="notification">
-          Edit the <code>./src</code> folder to add components.
+          <RenderTags />
         </div>
       </div>
     </div>
